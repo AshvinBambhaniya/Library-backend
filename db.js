27993cd485802a3a3d03d4fdc9fa7ee73004c986
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-03-08 17:43:37 +0530

    Backend are create

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://notebook-backend:notebook-backend@cluster0.efw5kzi.mongodb.net/library-backend?retryWrites=true&w=majority";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to mongoose Successfully")
+    })
+}
+
+module.exports = connectToMongo;
