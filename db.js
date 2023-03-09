97822f071c52ebfe4461447babc8230451bb882e
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-03-09 21:53:49 +0530

    Mogo url in env file

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://notebook-backend:notebook-backend@cluster0.efw5kzi.mongodb.net/library-backend?retryWrites=true&w=majority";
+require('dotenv').config();
+
+const mongoURI = process.env.MONGODB_URL;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
