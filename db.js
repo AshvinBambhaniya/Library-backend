583f324ae4335f4df1dd120ef0d10d788dad5b8b
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-08-06 23:24:48 +0530

    Add Book in backend

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://notebook-backend:notebook-backend@cluster0.efw5kzi.mongodb.net/library-backend?retryWrites=true&w=majority";
+const mongoURI = "mongodb://localhost:27017/Library";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- routes/book.js
+++ routes/book.js
@@ -38,12 +38,41 @@ router.get('/fetchallbook', fetchuser, async (req, res) => {
     }
 })
 
-router.delete('/deletebook', fetchuser, async (req, res) => {
+router.put('/updatebook/:id', fetchuser, async (req, res) => {
+
+    const { book, auther, publisher } = req.body;
+
+    try {
+        // Create a newNote object
+        const newBook = {};
+        if (book) { newBook.book = book };
+        if (auther) { newBook.auther = auther };
+        if (publisher) { newBook.publisher = publisher };
+
+        newBook.date = Date.now();
+
+        // Find the note to be updated and update it
+        let old_book = await Book.findById(req.params.id);
+        if (!old_book) { return res.status(404).send("Not Found") }
+
+        if (old_book.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        old_book = await Book.findByIdAndUpdate(req.params.id, { $set: newBook }, { new: true })
+        res.status(201).json({ old_book });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
+})
+
+router.delete('/deletebook/:id', fetchuser, async (req, res) => {
     try {
 
-        const { book, auther, publisher } = req.body;
-
-        let deletebook = await Book.findOne({ book, auther, publisher, user: req.user.id })
+        let deletebook = await Book.findById(req.params.id)
         if (!deletebook) {
             return res.status(404).send("Not Found")
         }
@@ -52,11 +81,12 @@ router.delete('/deletebook', fetchuser, async (req, res) => {
             return res.status(401).send("Not Allowed");
         }
 
-        deletebook = await Book.findByIdAndDelete(deletebook.id)
+        deletebook = await Book.findByIdAndDelete(req.params.id)
         res.status(201).json(deletebook);
 
     } catch (error) {
-
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
     }
 })
 
